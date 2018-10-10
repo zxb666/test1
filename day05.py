77a7b8d77719ec397f1585889da0d98194cc9a6a
root <root@localhost.localdomain>
2018-10-09 21:39:08 -0400

    day5

---
+++ day05.py
@@ -0,0 +1,104 @@
+'''
+class joker:
+    def __init__(self):
+        self.a=eval(raw_input("enter is shu:" ))
+        self.num=10000
+        def check(self):
+            if self%2==0:
+                print '[+] ou'
+            else:
+                print '[+]' ji
+
+
+# _*_conding:utf8-
+class  rectangle:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+    def getck(self):
+        print self.width,self.height
+    def getArea(self):
+        area=self.width*self.height
+        print area
+    def getp(self):
+        res2=2*(self.width+self.height)
+        print res2
+if __name__  == '__ main__':
+    rdeec=rectangle(4,40)
+    rec.getck()
+    rec.getArea()
+    rec.getp()
+
+ class ACCount:
+     def __ init__(int id=0,float balance=100,float annuallnterestRate=0):
+         self.id=id
+         self.balance=balance
+         self.annuallnterestRate=annuallnterestRate
+     def getMonthlyInterestRate(self):
+         mon=
+
+
+
+
+
+import math
+class RegularPolygon:
+    def __init__(self,n=3,side=1,x=0,y=0):
+        self.__n=n
+        self.__side=side
+        self.__x=x
+        self.__y=y
+    def getPerimeter(self):
+        zhongchang=self.__n*self.__side
+        print zhongchang
+    def getArea(self):
+        area=(self.__n*(self.__side*self.__side))/(4*math.tan(math.pi/self.__n))
+        print area
+if __name__=='__main__':
+    a1=RegularPolygon()
+    a2=RegularPolygon(6,4)
+    a3=RegularPolygon(10,4,5.6,7.8)
+    a1.getPerimeter()
+    a1.getArea()
+    a2.getPerimeter()
+    a2.getArea()
+    a3.getPerimeter()
+    a3.getArea()
+    
+
+    
+'''
+class Fan:
+    def __init__(speed=SLOW,radius=5,color=blue,on=false):
+        self.__speed=speed
+        self.__radius=radius
+        self.__color=color
+        self.__on=on
+    def A(self):
+        n1=Fan("Fast",true,10,"yellow")
+        print n1
+    def B(self):
+        n2=Fan("MEDIUM",false,5,"blue")
+        print n2
+if __name__=='__main__':
+    rec.A()
+    rec.B()
+
+
+'''
+class LinearEquation:
+    def __init__(self,a,b,c,d,e,f):
+        self.__a=a
+        self.__b=b
+        self.__c=c
+        self.__d=d
+        self.__e=e
+        self.__f=f
+    def isSolvable(self):
+
+
+
+
+
+
+
